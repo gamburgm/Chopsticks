645de2754a6f7bbb45a7f592fc5d13cbe86bacc3
gamburgm <mitch.gamburg@gmail.com>
2021-10-25 21:35:31 -0400

    search recursively correctly

--- chopsticks.py
+++ chopsticks.py
@@ -30,12 +30,6 @@ def is_game_over(state):
 def first_player_winner(state):
     return is_eliminated(state[1])
 
-def copy_player(player):
-    return (player[0], player[1])
-
-def copy_state(state):
-    return (copy_player(state[0]), copy_player(state[1]))
-
 def valid_hand(h):
     return 0 <= h < 5
 
@@ -70,17 +64,16 @@ def best_outcome(o1, o2):
 
 # GameState, Set[GameState], Map[GameState, Result] -> Result
 def solve(state, seen, known):
-    if is_game_over(state):
-        if first_player_winner(state):
-            # whatever
-            pass
-        else:
-            # also whatever
-            pass
-        return
-
     if state in known:
         return known[state]
+
+    if is_game_over(state):
+        if first_player_winner(state):
+            known[state] = Win()
+            return Win()
+        else:
+            known[state] = Lose()
+            return Lose()
 
     if state in seen:
         known[state] = Draw() # is this the right thing to do? also, TODO when copy, when not copy? every recursive call on new state?
@@ -89,20 +82,27 @@ def solve(state, seen, known):
     best_res = Lose()
 
     seen.add(state)
-    # subtract left from left
 
-    # how to check for validity? and where?
-    ll_move = ((inc_hand(state[1][0], state[0][0]), state[1][1]), copy_player(state[0]))
-    best_res = best_outcome(best_res, solve(ll_move, seen, known))
+    first_left_empty = empty_hand(state[0][0])
+    first_right_empty = empty_hand(state[0][1])
+    second_left_empty = empty_hand(state[1][0])
+    second_right_empty = empty_hand(state[1][1])
 
-    lr_move = ((state[1][0], inc_hand(state[1][1], state[0][0])), copy_player(state[0])) # I don't actually need copy player, do I?
-    best_res = best_outcome(best_res, solve(lr_move, seen, known))
+    if not first_left_empty and not second_left_empty:
+        ll_move = ((inc_hand(state[1][0], state[0][0]), state[1][1]), state[0])
+        best_res = best_outcome(best_res, flip_result(solve(ll_move, seen, known)))
 
-    rl_move = ((inc_hand(state[1][0], state[0][1]), state[1][1]), copy_player(state[0]))
-    best_res = best_outcome(best_res, solve(rl_move, seen, known))
+    if not first_left_empty and not second_right_empty:
+        lr_move = ((state[1][0], inc_hand(state[1][1], state[0][0])), state[0]) # I don't actually need copy player, do I?
+        best_res = best_outcome(best_res, flip_result(solve(lr_move, seen, known)))
 
-    rr_move = ((state[1][0], inc_hand(state[1][1], state[0][1])), copy_player(state[0]))
-    best_res = best_outcome(best_res, solve(rr_move, seen, known))
+    if not first_right_empty and not second_left_empty:
+        rl_move = ((inc_hand(state[1][0], state[0][1]), state[1][1]), state[0])
+        best_res = best_outcome(best_res, flip_result(solve(rl_move, seen, known)))
+
+    if not first_right_empty and not second_right_empty:
+        rr_move = ((state[1][0], inc_hand(state[1][1], state[0][1])), state[0])
+        best_res = best_outcome(best_res, flip_result(solve(rr_move, seen, known)))
 
     if can_split(state[0]):
         new_p = split_player(state[0])
@@ -112,15 +112,4 @@ def solve(state, seen, known):
     known[state] = best_res
     return best_res
 
-    # otherwise, try every combo
-    # there are 6 possible moves:
-    # 1. subtract left from left
-    # 2. subtract left from right
-    # 3. subtract right from left
-    # 4. subtract right from right
-    # 5. split when left is 2 
-    # 6. split when left is 4
-    # 7. split when right is 2
-    # 8. split when right is 4
-
-
+print(solve(((1, 1), (1, 1)), set(), {}))
